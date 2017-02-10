d48641f60631cc37e5154b5f77a7850c80a725af
Iulia Chiriac <iulia.chiriac@eaudeweb.ro>
2017-02-10 16:09:23 +0200

    [refs #81736] Fix alembic migrations.

--- alembic/sql/etc_data_birds.sql
+++ alembic/sql/etc_data_birds.sql
@@ -4,24 +4,10 @@
 -- ------------------------------------------------------
 -- Server version	5.6.17
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `etc_data_birds`
 --
 
-DROP TABLE IF EXISTS `etc_data_birds`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `etc_data_birds` (
   `country` varchar(8) NOT NULL,
   `country_isocode` varchar(4) DEFAULT NULL,
@@ -142,4 +128,3 @@ CREATE TABLE `etc_data_birds` (
   PRIMARY KEY (`country`,`speciescode`,`ext_dataset_id`),
   KEY `group` (`group`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-/*!40101 SET character_set_client = @saved_cs_client */;
