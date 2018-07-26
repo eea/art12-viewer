cd90fc5fbb0e323dbdb775f116144e45944d5845
Diana Boiangiu <boiangiudiana58@gmail.com>
2018-07-26 17:54:46 +0300

    [refs #97393] Modify greece import command for production environment

--- art12/management/import_greece.py
+++ art12/management/import_greece.py
@@ -18,22 +18,22 @@ class ImportGreeceCommand(BaseCreateUserCommand):
         speciescodes = [
             data.speciescode for data in
             models.EtcDataBird.query.all()
-            if data.dataset_id == 2
-        ]
-        species = [
-            data.speciesname for data in models.EtcDataBird.query.filter_by(
-                country_isocode='GR').filter_by(dataset_id=2)
+            if data.dataset_id == 2 and data.country_isocode == 'GR'
         ]
 
         for speciescode in speciescodes:
             ludatabird = models.LuDataBird.query.filter_by(
-                speciescode=speciescode,
-                dataset_id=2
+                speciescode=speciescode
             ).first()
-            if ludatabird:
-                if ludatabird.speciesname not in species:
-                    models.db.session.delete(ludatabird)
-            models.db.session.commit()
+            if not models.LuDataBird.query.filter_by(
+                    speciescode=ludatabird.speciescode, dataset_id=2).all():
+                new_ludatabird = models.LuDataBird(
+                    speciescode=ludatabird.speciescode,
+                    speciesname=ludatabird.speciesname,
+                    dataset_id=2
+                )
+                models.db.session.add(new_ludatabird)
+                models.db.session.commit()
 
 
 import_greece = Manager()
