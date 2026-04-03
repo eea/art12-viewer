044df45d3d64323868e4b50ce1fe99cecc861f9b
Diana Boiangiu <boiangiudiana58@gmail.com>
2026-04-03 16:13:44 +0300

    Extend file size

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -7,3 +7,6 @@ name = "article12"
 script_name = environ.get("SCRIPT_NAME", "")
 worker_refresh_batch_size = 0
 worker_class = "gevent"
+lmit_request_line = 0
+limit_request_fields = 1000
+limit_request_field_size = 0
